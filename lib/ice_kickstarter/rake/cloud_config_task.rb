2f47cff9cd83dfa1640ad7ed4d8dfe1457f8361c
Falk Köppe <falk.koeppe@infopark.de>
2013-02-23 13:14:23 +0100

    pseudo code for the improved process to only edit the custom_cloud section of the config file
    + code cleanup

--- lib/ice_kickstarter/rake/cloud_config_task.rb
+++ lib/ice_kickstarter/rake/cloud_config_task.rb
@@ -11,42 +11,69 @@ module IceKickstarter
       def initialize
         namespace :cms do
           namespace :cloud_config do
-
-            desc "Edit the cloud config as stored in the ICE Konsole"
+            desc 'Edit the cloud config as stored in the ICE Konsole'
             task :edit => :environment do
               edit
             end
 
-            desc "Download the cloud config from the ICE Konsole to edit it locally"
+            desc 'Download the cloud config from the ICE Konsole to edit it locally'
             task :download => :environment do
               download
             end
 
-            desc "Upload the edited cloud config to the ICE Konsole"
+            desc 'Upload the edited cloud config to the ICE Konsole'
             task :upload => :environment do
               upload
             end
-
           end
         end
       end
 
       private
 
+      # edit
+        # download
+        # if editor?
+          # open editor for "tmp/custom_cloud_config.json"
+        # else
+          # print message "edit 'tmp/custom_cloud_config.json' and run rake cms:cloud_config:upload"
+        # upload
+
+      # download
+        # download config via curl
+        # save config in "tmp/cloud_config.json"
+        # load config in "tmp/cloud_config.json"
+        # extract "custom_cloud" section
+        # save section in "tmp/custom_cloud_config.json"
+
+      # upload
+        # load "tmp/custom_cloud_config.json"
+        # load config in "tmp/cloud_config.json"
+        # exchange "custom_cloud" section
+        # save config in "tmp/cloud_config.json"
+        # upload config via curl
+        # backup in "tmp/cloud_config-1234567.json"
+
       def edit
         download
-        if ENV["EDITOR"].to_s != ""
-          sh "#{ENV["EDITOR"]} #{config_path}", verbose: false
+
+        if editor?
+          sh("#{ENV['EDITOR']} #{config_path}", verbose: false)
+
           upload
         else
-          puts "No $EDITOR specified."
+          puts 'No $EDITOR specified.'
           puts "Edit #{config_path}"
-          puts "Finally upload with rake cms:cloud_config:upload"
+          puts 'Finally upload with rake cms:cloud_config:upload'
         end
       end
 
+      def editor?
+        ENV['EDITOR'].to_s != ''
+      end
+
       def download
-        config = RestClient.get("#{url}/cloud_config", params: {token: api_key})
+        config = RestClient.get("#{url}/cloud_config", params: { token: api_key })
         save_config(config)
       end
 
@@ -54,18 +81,20 @@ module IceKickstarter
         config = load_config
         puts config
 
-        if yes?("Upload this config to the ICE Konsole? [yn]")
-          puts "Uploading..."
+        if yes?('Upload this config to the ICE Konsole? [yn]')
+          puts 'Uploading...'
+
           config_and_token = {
             token: api_key,
             cloud_config: config,
           }
+
           RestClient.put("#{url}/cloud_config", config_and_token.to_json, content_type: :json, accept: :json)
 
           puts "Local backup: #{config_backup_path}"
-          cp config_path, config_backup_path, verbose: false
+          cp(config_path, config_backup_path, verbose: false)
         else
-          puts "Aborting."
+          puts 'Aborting.'
         end
       rescue MultiJson::LoadError => e
         puts "ERROR: #{config_path} contains syntax errors. Please fix."
@@ -89,16 +118,21 @@ module IceKickstarter
       end
 
       def config_path
-        Rails.root + "tmp/cloud_config.json"
+        Rails.root + 'tmp/cloud_config.json'
       end
 
       def config_backup_path
-        @config_backup_path ||= "#{config_path}-#{Time.now.strftime('%Y%m%d%H%M')}"
+        @config_backup_path ||= "#{config_path}-#{current_time}"
+      end
+
+      def current_time
+        Time.now.strftime('%Y%m%d%H%M')
       end
 
       def yes?(text)
         puts text.to_s
-        $stdin.gets.tap{|text| text.strip! if text}.to_s =~ /^y/
+
+        $stdin.gets.tap { |text| text.strip! if text }.to_s =~ /^y/
       end
     end
   end
