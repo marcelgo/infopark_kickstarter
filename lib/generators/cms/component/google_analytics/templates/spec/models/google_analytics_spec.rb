509323f048b332ea0a8e89e41b896265bf77c888
Falk Köppe <falk.koeppe@infopark.de>
2013-03-13 14:04:28 +0100

    uses generated attributes instead of overriding the getters

--- lib/generators/cms/component/google_analytics/templates/spec/models/google_analytics_spec.rb
+++ lib/generators/cms/component/google_analytics/templates/spec/models/google_analytics_spec.rb
@@ -8,29 +8,29 @@ describe GoogleAnalytics do
   describe 'anonymize_ip' do
     it 'returns true on "Yes"' do
       subject.stub(:[]).with(:google_analytics_anonymize_ip).and_return('Yes')
-      subject.anonymize_ip?.should be_true
+      subject.google_analytics_anonymize_ip?.should be_true
     end
 
     it 'returns false on "NO"' do
       subject.stub(:[]).with(:google_analytics_anonymize_ip).and_return('No')
-      subject.anonymize_ip?.should be_false
+      subject.google_analytics_anonymize_ip?.should be_false
     end
 
     it 'returns false on nil' do
       subject.stub(:[]).with(:google_analytics_anonymize_ip).and_return(nil)
-      subject.anonymize_ip?.should be_false
+      subject.google_analytics_anonymize_ip?.should be_false
     end
   end
 
   describe 'tracking_id' do
     it 'returns 123 on 123' do
       subject.stub(:[]).with(:google_analytics_tracking_id).and_return('123')
-      subject.tracking_id.should eq('123')
+      subject.google_analytics_tracking_id.should eq('123')
     end
 
     it 'returns empty string on nil' do
       subject.stub(:[]).with(:google_analytics_tracking_id).and_return(nil)
-      subject.tracking_id.should eq('')
+      subject.google_analytics_tracking_id.should eq('')
     end
   end
 end
