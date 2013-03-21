b42ea2b57f985ac1af94447d5868d03af817dcd8
Falk Köppe <falk.koeppe@infopark.de>
2013-03-21 10:15:42 +0100

    limit blog posts to latest 10 on the overview page

--- lib/generators/cms/component/blog/templates/app/models/blog.rb
+++ lib/generators/cms/component/blog/templates/app/models/blog.rb
@@ -10,9 +10,13 @@ class Blog < Obj
 
   def latest_entries
     # TODO Currently only the published workspace is searchable.
-    # TODO Limit result size
     RailsConnector::Workspace.find('published').as_current do
-      BlogEntry.all.and(:_path, :starts_with, path + '/').batch_size(100).order(:_valid_from).reverse_order.to_a
+      BlogEntry.all.
+        and(:_path, :starts_with, path + '/').
+        batch_size(100).
+        order(:_valid_from).
+        reverse_order.
+        take(10)
     end
   end
 end
