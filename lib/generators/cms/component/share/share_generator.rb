d959704ef62187b201d5f8a3ebb8eec485caee41
Jan-Hendrik Heuing <jh@ivi-solutions.com>
2013-04-07 22:45:57 +0200

    initial setup social share plugin

---
+++ lib/generators/cms/component/share/share_generator.rb
@@ -0,0 +1,29 @@
+module Cms
+  module Generators
+    module Component
+      class ShareGenerator < ::Rails::Generators::Base
+        include Actions
+
+        source_root File.expand_path('../templates', __FILE__)
+
+        def copy_app_directory
+          directory('app', force: true)
+        end
+
+        def insert_share_code
+          file = 'app/views/layouts/application.html.haml'
+          insert_point = "            = yield"
+
+          data = []
+
+          data << "\n"
+          data << "            = render_cell(:share, :show)"
+
+          data = data.join("\n")
+
+          insert_into_file(file, data, after: insert_point)
+        end
+      end
+    end
+  end
+end
---
+++ lib/generators/cms/component/share/templates/app/cells/share_cell.rb
@@ -0,0 +1,45 @@
+class ShareCell < BoxCell
+  # Cell actions:
+
+  def show(url = '', provider = [])
+    if url.present?
+      @url = url
+    else
+      @url = request.url
+    end
+
+    if provider.present?
+      @provider = provider.find_all{|item| available_provider.include?(item); }
+    else
+      @provider = available_provider
+    end
+
+    render
+  end
+
+  def facebook
+    render
+  end
+
+  def google
+    render
+  end
+
+  def twitter
+    render
+  end
+
+  def xing
+    render
+  end
+
+  def linkedin
+    render
+  end
+
+  private
+
+  def available_provider
+    @valid_provider ||= [:facebook, :google, :twitter, :xing, :linkedin]
+  end
+end
