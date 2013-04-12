cb4f9666e3ec222f44734bac33f70c9ed11bb3a1
Alexander Schrot <alexander.schrot@infopark.de>
2013-04-12 09:23:11 +0200

    added provider support for the maps widget

---
+++ lib/generators/cms/widget/maps/google_maps/google_maps_generator.rb
@@ -0,0 +1,63 @@
+module Cms
+  module Generators
+    module Widget
+      module Maps
+        class GoogleMapsGenerator < ::Rails::Generators::Base
+          include Migration
+          include BasePaths
+
+          source_root File.expand_path('../templates', __FILE__)
+
+          def create_migration
+            begin
+              Model::ApiGenerator.new(behavior: behavior) do |model|
+                model.name = obj_class_name
+                model.title = 'Widget: GoogleMaps'
+                model.attributes = [
+                  {
+                    name: address_attribute_name,
+                    type: :string,
+                    title: 'Address',
+                  },
+                  {
+                    name: sort_key_attribute_name,
+                    type: :string,
+                    title: 'Sort key',
+                  }
+                ]
+              end
+            rescue Cms::Generators::DuplicateResourceError
+            end
+          end
+
+          def copy_app_directory
+            directory('app', force: true)
+            directory('spec', force: true)
+
+            template('thumbnail.html.haml', 'app/widgets/google_maps_widget/thumbnail.html.haml')
+          end
+
+          def notice
+            if behavior == :invoke
+              log(:migration, 'Make sure to run "rake cms:migrate" to apply CMS changes')
+            end
+          end
+
+          private
+
+          def obj_class_name
+            'GoogleMapsWidget'
+          end
+
+          def address_attribute_name
+            'address'
+          end
+
+          def sort_key_attribute_name
+            'sort_key'
+          end
+        end
+      end
+    end
+  end
+end
