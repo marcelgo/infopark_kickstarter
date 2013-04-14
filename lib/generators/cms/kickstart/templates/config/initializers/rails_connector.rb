190db972a878a403bd04aac0a911ceda2b123c27
Falk Köppe <falk.koeppe@infopark.de>
2013-04-14 16:53:29 +0200

    implements the editing_auth callback

--- lib/generators/cms/kickstart/templates/config/initializers/rails_connector.rb
+++ lib/generators/cms/kickstart/templates/config/initializers/rails_connector.rb
@@ -4,12 +4,10 @@ end
 
 # This callback is important for security.
 #
-# It is used to provide inplace editing features. Even if you don't use inplace
-# editing on the client side, the server side also uses this callback to
-# determine if CMS data can be modified in the database.
+# It is used to provide inplace editing features. Even if you don't use inplace editing
+# on the client side, the server side also uses this callback to determine if CMS data
+# can be modified in the database.
 RailsConnector::Configuration.editing_auth do |env|
   request = Rack::Request.new(env)
-
-  # for simplicity just return true. NEVER EVER USE THIS IN PRODUCTION!!!
-  true
+  EditModeDetection.editing_allowed?(request.session)
 end
