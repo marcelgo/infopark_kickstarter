e7b098ea17501af2f2f8975015efd3c5ca384ce1
Falk Köppe <falk.koeppe@infopark.de>
2013-04-21 11:11:35 +0200

    comments out force https, so it can be easily enabled when necessary

--- lib/generators/cms/kickstart/templates/app/controllers/application_controller.rb
+++ lib/generators/cms/kickstart/templates/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  include ForceHttps
+  # include ForceHttps
   include EditModeDetection
   include WorkspaceSelection
   include ExceptionHandling
