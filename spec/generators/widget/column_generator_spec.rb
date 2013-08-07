ade15eb02e8c9adf865037dd506b262894704626
Falk Köppe <falk.koeppe@infopark.de>
2013-08-07 14:08:45 +0200

    update to also check for edit view

--- spec/generators/widget/column_generator_spec.rb
+++ spec/generators/widget/column_generator_spec.rb
@@ -29,6 +29,7 @@ describe Cms::Generators::Widget::ColumnGenerator do
 
             directory 'views' do
               file 'show.html.haml'
+              file 'edit.html.haml'
               file 'thumbnail.html.haml'
             end
           end
