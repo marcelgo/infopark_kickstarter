6006e4fab42a8826db40aacffaed53775f6a5a12
Robert Wachs <robert.wachs@infopark.de>
2013-08-28 10:39:14 +0200

    column generator: uses icon that depends on the number of columns

--- spec/generators/widget/column_generator_spec.rb
+++ spec/generators/widget/column_generator_spec.rb
@@ -7,50 +7,70 @@ describe Cms::Generators::Widget::ColumnGenerator do
   include GeneratorSpec::TestCase
 
   destination File.expand_path('../../../../tmp/generators', __FILE__)
-  arguments ['--columns=3', '--max_columns=9', '--example']
 
   before do
     prepare_destination
-    run_generator
   end
 
-  it 'creates files' do
-    destination_root.should have_structure {
-      directory 'app' do
-        directory 'widgets' do
-          directory 'column3_widget' do
-            directory 'locales' do
-              file 'en.column3_widget.yml' do
-                contains 'column3_widget'
-                contains 'notice:'
+  describe 'with three columns' do
+    before do
+      run_generator ['--columns=3', '--max_columns=9', '--example']
+    end
+
+    it 'creates files' do
+      destination_root.should have_structure {
+        directory 'app' do
+          directory 'widgets' do
+            directory 'column3_widget' do
+              directory 'locales' do
+                file 'en.column3_widget.yml' do
+                  contains 'column3_widget'
+                  contains 'notice:'
+                end
+              end
+
+              directory 'migrate' do
+                migration 'create_column3_widget'
+                migration 'create_column3_widget_example'
+              end
+
+              directory 'views' do
+                file 'show.html.haml'
+                file 'edit.html.haml'
+                file 'thumbnail.html.haml' do
+                  contains 'widget-icon-3cols'
+                end
               end
             end
+          end
 
-            directory 'migrate' do
-              migration 'create_column3_widget'
-              migration 'create_column3_widget_example'
-            end
-
-            directory 'views' do
-              file 'show.html.haml'
-              file 'edit.html.haml'
-              file 'thumbnail.html.haml'
+          directory 'models' do
+            file 'column3_widget.rb' do
+              contains 'cms_attribute :column_1, type: :widget'
+              contains 'cms_attribute :column_2, type: :widget'
+              contains 'cms_attribute :column_3, type: :widget'
+              contains 'cms_attribute :column_1_width, type: :string, default: \'3\''
+              contains 'cms_attribute :column_2_width, type: :string, default: \'3\''
+              contains 'cms_attribute :column_3_width, type: :string, default: \'3\''
+              contains 'include Widget'
             end
           end
         end
+      }
+    end
+  end
 
-        directory 'models' do
-          file 'column3_widget.rb' do
-            contains 'cms_attribute :column_1, type: :widget'
-            contains 'cms_attribute :column_2, type: :widget'
-            contains 'cms_attribute :column_3, type: :widget'
-            contains 'cms_attribute :column_1_width, type: :string, default: \'3\''
-            contains 'cms_attribute :column_2_width, type: :string, default: \'3\''
-            contains 'cms_attribute :column_3_width, type: :string, default: \'3\''
-            contains 'include Widget'
-          end
+  describe 'with one column' do
+    before do
+      run_generator ['--columns=1', '--example']
+    end
+
+    it 'uses icon-class which is not pluralized' do
+      destination_root.should have_structure {
+        file 'app/widgets/column1_widget/views/thumbnail.html.haml' do
+          contains 'widget-icon-1col'
         end
-      end
-    }
+      }
+    end
   end
 end
